ee9a1e42000c6d24477541a145e56b322aa8cc92
victor <viktorkh@gmail.com>
2023-07-05 11:29:00 +0300

    added sceleton for aws web-app

---
+++ tr_web_aws/main.tf
@@ -0,0 +1,41 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "security_group" {
+  source = "./modules/security_group"
+}
+
+module "rds" {
+  source = "./modules/rds"
+}
+
+module "alb" {
+  source = "./modules/alb"
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+  alb_target_group_arn = module.alb.target_group_arn
+}
+
+output "alb_dns_name" {
+  value = module.alb.alb_dns_name
+}
+
+output "rds_instance_id" {
+  value = module.rds.rds_instance_id
+}
